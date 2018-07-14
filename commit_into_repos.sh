d0c97cee6c09f6a5d78a4d6f5b680801fd6169bd
Dmitry Ledentsov <dmlled@yahoo.com>
2018-07-14 15:58:03 +0200

    fix committing into the repo

--- commit_into_repos.sh
+++ commit_into_repos.sh
@@ -1,3 +1,6 @@
+#!/bin/bash
+set -e
+
 echo "--== deleting tmp ==--"
 rm -rf tmp
 
@@ -8,12 +11,15 @@ for NAME in a b c d helper with_externals
 do
     echo "--== setting up the ${NAME} repo ==--"
     svn co http://localhost:18080/svn/${NAME} tmp/${NAME} --username=admin --password=admin
+    mkdir test/svn-data/repositories/${NAME}/db/txn-protorevs || true
+    mkdir test/svn-data/repositories/${NAME}/db/transactions || true
     touch tmp/${NAME}/${NAME}
-    svn add tmp/${NAME}/*
+    date >> tmp/${NAME}/${NAME}
+    svn add tmp/${NAME}/* --force
     svn commit tmp/${NAME} -m "first commit in ${NAME}"
 done
 
 svn up tmp/with_externals
-svn propset svn:externals "helper http://svn-server:18080/svn/helper" tmp/with_externals
-svn up tmp/with_externals
-svn commit -m 'added external helper' tmp/with_externals
+svn propset svn:externals "helper http://svn-server:18080/svn/helper" tmp/with_externals || true
+svn up tmp/with_externals || true
+svn commit -m 'added external helper' tmp/with_externals || true
